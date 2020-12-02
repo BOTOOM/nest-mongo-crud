c3753506240dcfd8801621ee7bea8b49cfbf11d7
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-12-01 22:51:59 -0500

    adicion de modulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { HeroesModule } from './modules/heroes/heroes.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27018/nest_prueba')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27018/nest_prueba'),
+     HeroesModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/heroes/heroes.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('heroes')
+export class HeroesController {}
