e93df2cf3d94237b31e4d6f9a186c788342cce3f
BOTOOM <eddiazr@correo.udistrital.edu.co>
2020-12-01 22:59:08 -0500

    adicion de endpoints

--- src/modules/heroes/heroes.controller.ts
+++ src/modules/heroes/heroes.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get, Put, Delete, Param, Body } from '@nestjs/common';
 
 @Controller('heroes')
-export class HeroesController {}
+export class HeroesController {
+    constructor() {
+
+    }
+
+    @Get('GetAll')
+    getAll() {
+    }
+
+    @Get(':idRegistro')
+    getOne( @Param('idRegistro') id: string ) {
+    }
+
+    @Post()
+    post(@Body() datos: any) {
+    }
+
+    @Put(':idRegistro')
+    put( @Param('idRegistro') id: string, @Body() datos: any ) {
+    }
+
+    @Delete(':idRegistro')
+    delete( @Param('idRegistro') id: string ) {
+    }
+
+}
